cc1abb4c50034ec8442b188769d6973cc5c98106
ShuttlerBad <dileepasudara@gmail.com>
2024-10-28 11:41:00 +0530

    add residence to favourites list

--- server/controllers/userCntrl.js
+++ server/controllers/userCntrl.js
@@ -91,3 +91,38 @@ export const createUser = asyncHandler(async (req, res) => {
         throw new Error(err.message);
     }
   })
+
+  // add residence to your favourite list
+  export const toFav = asyncHandler(async(req, res) => {
+    const {email} = req.body;
+    const {rid} = req.params;
+
+    try{
+        const user = await prisma.user.findUnique({
+            where: {email}
+        })
+        if(user.favResidenciesID.includes(rid)){
+            const updateUser = await prisma.user.update({
+                where: {email},
+                data: {
+                    favResidenciesID:{
+                        set: user.favResidenciesID.filter((id) => id !== rid)
+                    }
+                }
+            })
+            res.send({message: "Removed from favourites", user: updateUser})
+        }else{
+            const updateUser = await prisma.user.update({
+                where: {email},
+                data: {
+                    favResidenciesID:{
+                        push: rid
+                    }
+                }
+            })
+            res.send({message: "Updated favourites", user: updateUser})
+        }
+    }catch(err){
+        throw new Error(err.message);
+    }
+  })
