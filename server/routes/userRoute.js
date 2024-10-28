522043ae52be504ae1b2c2487ddb16e07593e456
ShuttlerBad <dileepasudara@gmail.com>
2024-10-28 11:44:09 +0530

    routes updated to add favourite residence

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { bookVisit, cancelBooking, createUser, getAllBookings } from "../controllers/userCntrl.js";
+import { bookVisit, cancelBooking, createUser, getAllBookings, toFav } from "../controllers/userCntrl.js";
 const router = express.Router()
 
 router.post("/register", createUser)
 router.post("/bookVisit/:id", bookVisit)
 router.post("/allBookings", getAllBookings)
 router.post("/removeBooking/:id", cancelBooking)
+router.post("/toFav/:rid", toFav)
 
 export {router as userRoute}
