3724532ecd3bc332b21cc736c50684de3b546972
ShuttlerBad <dileepasudara@gmail.com>
2024-11-11 17:18:47 +0530

    Hero section added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
 import Header from "./components/Header/Header"
+import Hero from "./components/Hero/hero"
+import './App.css'
+
 
 function App() {
   return(
     <div className="App">
-      <Header/>
+      <div>
+        <div className="white-gradient"/>
+        <Header/>
+        <Hero/>
+      </div>
     </div>
   )
 }
