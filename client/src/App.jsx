5f43037ac447011fad0170186082d8e764cae6fb
ShuttlerBad <dileepasudara@gmail.com>
2024-11-30 19:06:09 +0530

    modifications & updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,30 @@
-import Header from "./components/Header/Header"
-import Hero from "./components/Hero/hero"
+import { Suspense } from 'react';
 import './App.css'
-import Companies from "./components/Companies/Companies"
-import Residencies from "./components/Residencies/Residencies"
-import Value from "./components/Value/Value"
-import Contact from "./components/Contact/Contact"
-import GetStarted from "./components/GetStarted/GetStarted"
-import Footer from "./components/Footer/Footer"
+import Website from "./pages/Website"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from "./components/Layout/Layout"
+import Properties from './pages/Properties/Properties';
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 
 function App() {
+
+  const queryClient = new QueryClient()
   return(
-    <div className="App">
-      <div>
-        <div className="white-gradient"/>
-        <Header/>
-        <Hero/>
-      </div>
-      <Companies/>
-      <Residencies/>
-      <Value/>
-      <Contact/>
-      <GetStarted/>
-      <Footer/>
-    </div>
-  )
+    <QueryClientProvider client = {queryClient}>
+    <BrowserRouter>
+    <Suspense fallback = {<div>Loading...</div>}>
+      <Routes>
+
+        <Route element = {<Layout/>}>
+          <Route path="/" element = {<Website/>} />
+          <Route path="/properties" element = {<Properties/>} />
+        </Route>
+      </Routes>
+    </Suspense>
+    </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export default App
--- client/src/components/Residencies/Residencies.jsx
+++ client/src/components/Residencies/Residencies.jsx
@@ -4,6 +4,7 @@ import "swiper/css"
 import './Residencies.css'
 import data from '../../utils/slider.json'
 import { sliderSettings } from '../../utils/common';
+import PropertyCard from '../PropertyCard/PropertyCard'
 
 const Residencies = () => {
   return (
@@ -18,17 +19,7 @@ const Residencies = () => {
                 <SliderButtons/>
                 {data.map((card, i) => (
                         <SwiperSlide key={i}>
-                            <div className="flexColStart r-card">
-                                <img src={card.image} alt="home" />
-
-                                <span className="secondaryText r-price">
-                                    <span style={{color: 'orange'}}>$</span>
-                                    <span>{card.price}</span>
-                                </span>
-
-                                <span className="primaryText">{card.name}</span>
-                                <span className="secondaryText">{card.detail}</span>
-                            </div>
+                            <PropertyCard card = {card}/>
                         </SwiperSlide>
                     ))
                 }
