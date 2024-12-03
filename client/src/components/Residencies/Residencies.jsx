bdb9b5c919bb162b71ed6bccdbadce4f4e8119fd
ShuttlerBad <dileepasudara@gmail.com>
2024-12-03 17:24:03 +0530

    modifications

--- client/src/components/Residencies/Residencies.jsx
+++ client/src/components/Residencies/Residencies.jsx
@@ -5,8 +5,33 @@ import './Residencies.css'
 import data from '../../utils/slider.json'
 import { sliderSettings } from '../../utils/common';
 import PropertyCard from '../PropertyCard/PropertyCard'
+import useProperties from '../hooks/useProperties'
+import { PuffLoader } from 'react-spinners'
 
 const Residencies = () => {
+
+    const {data, isError, isLoading} = useProperties()
+
+    if(isError){
+        return(
+            <div className="wrapper">
+                <span>Error while fetching data</span>
+            </div>
+        )
+    }
+    if(isLoading){
+        return(
+            <div className="wrapper flexCenter" style={{height: "60vh"}}>
+                <PuffLoader
+                height="80"
+                width="80"
+                radius={1}
+                color="#4066ff"
+                aria-label="puff-loading"
+                />
+            </div>
+        )
+    }
   return (
     <section className="r-wrapper">
         <div className="paddings innerWidth r-container">
@@ -17,7 +42,7 @@ const Residencies = () => {
 
             <Swiper {...sliderSettings}>
                 <SliderButtons/>
-                {data.map((card, i) => (
+                {data.slice(0,8).map((card, i) => (
                         <SwiperSlide key={i}>
                             <PropertyCard card = {card}/>
                         </SwiperSlide>
